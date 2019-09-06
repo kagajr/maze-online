5d9d791ca13d106f6b88f0f465250c0ac187f2e5
kagajr <kagajr.hm@gmail.com>
2019-09-06 22:58:48 +0700

    :q

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # maze-online
 
 An online multiple players maze game.
@@ -18,3 +19,15 @@ Stage 3: Make it online.
 //TODO + movement inside maze
 //TODO + add start and end of the game
 //TODO + support multiple players
+=======
+# maze-online
+
+An online multiple players maze game.
+
+Stage 1: Generate maze.
+  + Status: 80%.
+Stage 2: Playable.
+  + Status: 0%.
+Stage 3: Make it online.
+  + Status: 0%.
+>>>>>>> :q
--- scripts/maze.js
+++ scripts/maze.js
@@ -1,63 +1,198 @@
-function maze(x,y) {
-	var n=x*y-1;
-	if (n<0) {alert("illegal maze dimensions");return;}
-	var horiz =[]; for (var j= 0; j<x+1; j++) horiz[j]= [],
-	    verti =[]; for (var j= 0; j<x+1; j++) verti[j]= [],
-	    here = [Math.floor(Math.random()*x), Math.floor(Math.random()*y)],
-	    path = [here],
-	    unvisited = [];
-	for (var j = 0; j<x+2; j++) {
-		unvisited[j] = [];
-		for (var k= 0; k<y+1; k++)
-			unvisited[j].push(j>0 && j<x+1 && k>0 && (j != here[0]+1 || k != here[1]+1));
-	}
-	while (0<n) {
-		var potential = [[here[0]+1, here[1]], [here[0],here[1]+1],
-		    [here[0]-1, here[1]], [here[0],here[1]-1]];
-		var neighbors = [];
-		for (var j = 0; j < 4; j++)
-			if (unvisited[potential[j][0]+1][potential[j][1]+1])
-				neighbors.push(potential[j]);
-		if (neighbors.length) {
-			n = n-1;
-			next= neighbors[Math.floor(Math.random()*neighbors.length)];
-			unvisited[next[0]+1][next[1]+1]= false;
-			if (next[0] == here[0])
-				horiz[next[0]][(next[1]+here[1]-1)/2]= true;
-			else 
-				verti[(next[0]+here[0]-1)/2][next[1]]= true;
-			path.push(here = next);
-		} else 
-			here = path.pop();
-	}
-	return {x: x, y: y, horiz: horiz, verti: verti};
+let canvas = document.getElementById('canvas');
+
+let x = y = 32;
+
+var road_size = 20;
+
+var m = [];
+
+var stack = [];
+
+var s1 = [];
+
+var s2 = [];
+
+var path = [];
+
+var ctx = canvas.getContext('2d');
+
+// ctx.strokeRect(20, 20, 640, 640);
+ctx.strokeStyle = 'green';
+
+for (let i = 1; i <= 32; i++) {
+    m[i] = [];
+    for (let j = 1; j <= 32; j++) {
+        m[i][j] = 'unvisited';
+        s1.push([i, j]);
+        ctx.beginPath();
+        ctx.strokeRect(i * 20, j * 20, 20, 20);
+    }
 }
- 
-function display(m) {
-	var text= [];
-	for (var j= 0; j<m.x*2+1; j++) {
-		var line= [];
-		if (0 == j%2)
-			for (var k=0; k<m.y*4+1; k++)
-				if (0 == k%4) 
-					line[k]= '+';
-				else
-					if (j>0 && m.verti[j/2-1][Math.floor(k/4)])
-						line[k]= ' ';
-					else
-						line[k]= '-';
-		else
-			for (var k=0; k<m.y*4+1; k++)
-				if (0 == k%4)
-					if (k>0 && m.horiz[(j-1)/2][k/4-1])
-						line[k]= ' ';
-					else
-						line[k]= '|';
-				else
-					line[k]= ' ';
-		if (0 == j) line[1]= line[2]= line[3]= ' ';
-		if (m.x*2-1 == j) line[4*m.y]= ' ';
-		text.push(line.join('')+'\r\n');
-	}
-	return text.join('');
+
+function hasUnvisitedCell() {
+    return s1.length > 0;
+}
+
+function markVisit(i, j) {
+    if (i <= 0 ||
+        j <= 0 ||
+        i > x ||
+        j > y) {
+        console.log('wrong index');
+        return;
+    }
+    stack.push([i, j]);
+    path.push([i, j]);
+    m[i][j] = 'visited';
+    s1.splice(s1.findIndex(e => e[0] === i && e[1] === j), 1);
+    return;
+}
+
+//TODO mark cell as visited
+function mazeGenerator() {
+    while (hasUnvisitedCell()) {
+        var randomCell = s1[getRandomArbitrary(0, s1.length)];
+        var initX = randomCell[0];
+        var initY = randomCell[1];
+        generateWall(initX, initY);
+
+    }
+
+    function generateWall(i, j) {
+        markVisit(i, j);
+        var nA = getNeighbor(i, j);
+        if (nA === 'out of neighborhood') {
+            console.log('out of stack!');
+            return;
+        } else {
+            removeWall(i, j, nA[0], nA[1]);
+            generateWall(nA[0], nA[1]);
+        }
+    }
+
+    function getNeighbor(x, y) {
+        if (hasNeighbor(x, y)) {
+            return getRandomNeighbor(x, y);
+        } else {
+
+            let newCell = stack.pop();
+            if (newCell != undefined) {
+                return getNeighbor(newCell[0], newCell[1]);
+                
+            } else {
+                s2.push(path.slice());
+                path = [];
+                return 'out of neighborhood';
+            }
+        }
+    }
+}
+
+mazeGenerator();
+
+console.log('maze generate completed');
+
+function hasNeighbor(x, y) {
+    if (s1.findIndex(e => e[0] === x + 1 && e[1] === y) != -1 ||
+        s1.findIndex(e => e[0] === x && e[1] === y + 1) != -1 ||
+        s1.findIndex(e => e[0] === x - 1 && e[1] === y) != -1 ||
+        s1.findIndex(e => e[0] === x && e[1] === y - 1) != -1) {
+        return true;
+    }
+    return false;
+}
+
+function getRandomNeighbor(i, j) {
+    var nX, nY;
+    do {
+        nX = 0;
+        nY = 0;
+        let direction = getRandomArbitrary(1, 5);
+        switch (direction) {
+            case 1:
+                nX = i;
+                nY = j - 1;
+                break;
+            case 2:
+                nX = i + 1;
+                nY = j;
+                break;
+            case 3:
+                nX = i;
+                nY = j + 1;
+                break;
+            case 4:
+                nX = i - 1;
+                nY = j;
+                break;
+
+        }
+    } while (!exist(nX, nY)  || nX <= 0 || nY <= 0 || nX > x || nY > y);
+    return [nX, nY];
+}
+
+function exist(i, j) {
+    return s1.findIndex(e => e[0] === i && e[1] === j) !== -1;
+}
+
+function removeWall(a, b, c, d) {
+    ctx.strokeStyle = 'red';
+    if (a > c) {
+        ctx.beginPath();
+        ctx.clearRect(a * 20 - 5, b * 20 + 1, 10, 18);
+        ctx.stroke();
+        console.log('remove east');
+
+    } else if (a < c) {
+        ctx.beginPath();
+        ctx.clearRect(a * 20 + 15, b * 20 + 1, 10, 18);
+        ctx.stroke();
+        console.log('remove west');
+    } else if (b > d) {
+        ctx.beginPath();
+        ctx.clearRect(a * 20 + 1, b * 20 - 5, 18, 10);
+        ctx.stroke();
+        console.log('remove north');
+    } else {
+        ctx.beginPath();
+        ctx.clearRect(a * 20 + 1, b * 20 + 15, 18, 10);
+        ctx.stroke();
+        console.log('remove south');
+    }
+}
+
+function drawMaze(x, y, w, h) {
+    if (w / 2 <= 20 || h / 2 <= 20) {
+        return;
+    }
+    let doorX = getRandomArbitrary(0, w / 10);
+    let doorY = getRandomArbitrary(0, h / 10);
+    let cX = w / 2;
+    let cY = h / 2;
+    let iX = x + cX;
+    let iY = y + cY;
+
+    ctx.beginPath();
+    ctx.moveTo(iX, y);
+    ctx.lineTo(iX, y + 10 * (doorY - 1));
+    ctx.moveTo(iX, y + 10 * (doorY + 1));
+    ctx.lineTo(iX, y + h);
+    ctx.moveTo(x, iY);
+    ctx.lineTo(x + 10 * (doorX - 1), iY);
+    ctx.moveTo(x + 10 * (doorX + 1), iY);
+    ctx.lineTo(x + w, iY);
+    ctx.stroke();
+
+    drawMaze(x, y, cX, cY);
+    drawMaze(iX, y, cX, cY);
+    drawMaze(x, iY, cX, cY);
+    drawMaze(iX, iY, cX, cY);
+
+}
+
+
+function getRandomArbitrary(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
